5598108d9632ed43c0c3464846512dd6dc80931a
zFlxw <flxw@venterion.com>
2021-08-16 23:58:53 +0200

    Added config system

---
+++ src/main/java/com/github/zflxw/reportreborn/utils/config/Config.java
@@ -0,0 +1,128 @@
+package com.github.zflxw.reportreborn.utils.config;
+
+import com.github.zflxw.reportreborn.ReportReborn;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.craftbukkit.libs.org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+public class Config {
+    private final File file;
+    private final YamlConfiguration yamlConfiguration;
+
+    public Config(File file) {
+        this.file = file;
+        this.yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+
+        this.load();
+    }
+
+    /**
+     * get a string from the config, with alternative color codes replaced
+     * @param key the key to search for
+     * @return the colored string
+     */
+    public String getColoredString(String key) {
+        return ChatColor.translateAlternateColorCodes('&', this.yamlConfiguration.getString(key));
+    }
+
+    /**
+     * get a string from the config
+     * @param key the key to search for
+     * @return the string of the key
+     */
+    public String getString(String key) {
+        return this.yamlConfiguration.getString(key);
+    }
+
+    /**
+     * get an int from the config
+     * @param key the key to search for
+     * @return the int of the key
+     */
+    public int getInt(String key) {
+        return this.yamlConfiguration.getInt(key);
+    }
+
+    /**
+     * get a double from the config
+     * @param key the key to search for
+     * @return the double of the key
+     */
+    public double getDouble(String key) {
+        return this.yamlConfiguration.getDouble(key);
+    }
+
+    /**
+     * get a boolean from the config
+     * @param key the key to search for
+     * @return the boolean of the key
+     */
+    public boolean getBoolean(String key) {
+        return this.yamlConfiguration.getBoolean(key);
+    }
+
+    /**
+     * get the config file
+     * @return the config file
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /**
+     * get the yaml configuration
+     * @return the yaml configuration
+     */
+    public YamlConfiguration getYamlConfiguration() {
+        return this.yamlConfiguration;
+    }
+
+    /**
+     * loads the default config values into the given file, if not present. Otherwise, the config will be checked and validated.
+     */
+    private void load() {
+        if (!file.exists()) {
+            Bukkit.getLogger().log(Level.WARNING, "No configuration file found. Loading default config.");
+            try {
+                Files.copy(ReportReborn.class.getClassLoader().getResourceAsStream("config.yml"), Paths.get(file.toURI()));
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        } else {
+            validateConfig(file);
+        }
+    }
+
+    /**
+     * check the config file with the entries from the template config. Missing entries will be added to the config file
+     * redundant entries in the config file will be ignored.
+     * @param file the config file
+     */
+    private void validateConfig(File file) {
+        try {
+            File tmpFile = new File(ReportReborn.getInstance().getDataFolder() + "/temp", "config-tmpl.yml");
+            FileUtils.copyInputStreamToFile(ReportReborn.class.getClassLoader().getResourceAsStream("config.yml"), tmpFile);
+
+            YamlConfiguration fileConfig = YamlConfiguration.loadConfiguration(file);
+            YamlConfiguration templateConfig = YamlConfiguration.loadConfiguration(tmpFile);
+
+            for (String key : templateConfig.getKeys(true)) {
+                if (!fileConfig.contains(key)) {
+                    fileConfig.set(key, templateConfig.get(key));
+                }
+            }
+
+            fileConfig.save(file);
+            FileUtils.deleteDirectory(new File(ReportReborn.getInstance().getDataFolder() + "/temp"));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
